d9ac7c550099044027370932d371cde206fb3d84
Juan Leal <jjlealmasa@gmail.com>
2022-10-16 09:39:26 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () =>  (
+  <div className='justify-self-start bg-purple-400 w-full overflow-hidden'>
+    <h1 className='text-5xl'>Hello World</h1>
+  </div>
+)
+
+
+
+export default App
